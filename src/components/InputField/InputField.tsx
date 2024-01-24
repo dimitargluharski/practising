ec435e99d5545db4cf7f089c35b6ffa34dc0d62e
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-01-24 22:45:31 +0200

    add debouncing

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,14 +1,25 @@
+// Now you can use InputField in this module
+
 interface InputFieldProps {
     onSearchTermChange: (searchTerm: string) => void;
 }
 
 const InputField = ({ onSearchTermChange }: InputFieldProps) => {
+    // @ts-ignore
+    let timerId: NodeJS.Timeout | null = null;
+
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onSearchTermChange(event.target.value);
+        if (timerId) {
+            clearTimeout(timerId);
+        }
+
+        timerId = setTimeout(() => {
+            onSearchTermChange(event.target.value);
+        }, 500);
     }
 
     return (
-        <input name="searchTerm" onChange={onChange} className="p-2 mt-2 mb-1 shadow-sm rounded-md focus:shadow-xl focus:scale-105 transition-all ease-linear outline-none" placeholder="Search..." />
+        <input name="searchTerm" onChange={onChange} className="p-2 mt-2 mb-1 shadow-sm rounded-md focus:shadow-xl focus:scale-105 transition-all ease- outline-none" placeholder="Search..." />
     )
 }
 
