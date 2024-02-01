634b00e8115b86926a370df3e7ef8c69915b057e
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-02-01 21:04:27 +0200

    add navbar
    add videos page
    add input field for search as well

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Live from './pages/Live/Live';
 import MatchDetails from './pages/MatchDetails/MatchDetails';
 import Stream from './components/Stream/Stream';
+import Navbar from './components/Navbar';
 // import Highlight from './components/Highlights/Highlight';
 
 function App() {
@@ -10,10 +11,12 @@ function App() {
     <div className='bg-slate-200 w-full h-[100%] flex justify-center'>
       <div className='w-[1024px] h-[100vh]'>
         {/* <div className='flex flex-col bg-slate-300 p-2 mr-2 rounded-md'> */}
+
         <div className='w-full p-2 flex items-center justify-center min-h-screen'>
+          <Navbar />
           <Routes>
             <Route path='/' element={<Live />} />
-            <Route path='/streams' element={<Stream />} />
+            <Route path='/videos' element={<Stream />} />
             <Route path='*' element={<div>404...</div>} />
             <Route path="/match-details/:matchId" element={<MatchDetails />} />
           </Routes>
--- src/components/Stream/Stream.tsx
+++ src/components/Stream/Stream.tsx
@@ -1,18 +1,33 @@
 import { useState, useEffect, useCallback } from 'react';
-import { Link } from 'react-router-dom';
 
 import { getHighlights } from '../../services/scoreBat';
+import Pagination from '../Pagination/Pagination';
+import InputField from '../InputField/InputField';
 
 const Stream = () => {
     // @ts-ignore
     const [liveStream, setLiveStream] = useState([]);
     const [high, setHigh] = useState([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const matchesPerPage: number = 9;
+
+    const onSearchTermChange = useCallback((term: string) => {
+        setSearchTerm(term);
+    }, []);
 
     const fetchHighlights = useCallback(async () => {
+        setLoading(true);
         const data = await getHighlights();
 
         // @ts-ignore
-        const sortedArray = data.liveStream.sort((a, b) => a.date.localeCompare(b.date));
+        const sortedArray = data.liveStream.sort((a, b) => {
+            const [hourA, minuteA] = a.date.split(':').map(Number);
+            const [hourB, minuteB] = b.date.split(':').map(Number);
+            return hourB - hourA || minuteB - minuteA;
+        });
+
         const sortedHigh = data.highlightVideos.sort((a, b) => {
             const [hourA, minuteA] = a.date.split(':').map(Number);
             const [hourB, minuteB] = b.date.split(':').map(Number);
@@ -23,37 +38,69 @@ const Stream = () => {
         // highlightVideos(highlightVideos)
         // @ts-ignore
         setHigh(sortedHigh);
+        setLoading(false);
     }, []);
 
     useEffect(() => {
         fetchHighlights();
     }, [fetchHighlights]);
 
-    console.log(high);
+
+
+    const filteredMatches = high.filter(match =>
+        match.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        match.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+
+    const lastPageIndex = currentPage * matchesPerPage;
+    const firstPageIndex = lastPageIndex - matchesPerPage;
+    const currentSlicedPage = filteredMatches.slice(firstPageIndex, lastPageIndex);
 
     return (
-        <div className='sm:grid sm:grid-cols-1 w-[768px]'>
-            <Link to="/">back</Link>
-            <div className='sm:grid sm:grid-cols-3 gap-2'>
+        <div className='flex flex-col w-[768px]'>
+            <InputField
+                onSearchTermChange={onSearchTermChange}
+            />
 
-                {high.slice(0, 35).map((stream: any) => (
-                    <div key={stream.title} className="max-w-md mx-auto bg-white rounded-xl shadow-md ">
-                        <div className="p-2 flex items-center justify-between">
-                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{stream.title}</div>
-                            <div>{stream.date}</div>
-                        </div>
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <div className='flex flex-col'>
+                    <div className='grid grid-cols-3 gap-5'>
+                        {currentSlicedPage.length > 0 ? currentSlicedPage.map((stream: any, index: number) => (
+                            <div key={index} className="max-w-md mx-auto bg-white rounded-xl shadow-md ">
+                                <div className="p-2 flex items-center justify-between">
+                                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                                        {stream.title.slice(0, 20)}...
+                                    </div>
+                                </div>
 
-                        <div className="p-2">
-                            <div className="w-[235px]">
-                                {/* <div dangerouslySetInnerHTML={{ __html: stream.embed }} className="h-full w-full" /> */}
-                                <iframe width={235} height={150} allowFullScreen={true} src={stream.videoSrc} className='w-full' />
+                                <div className="p-2">
+                                    <div className="w-[235px]">
+                                        <iframe width={235} height={150} allowFullScreen={true} src={stream.videoSrc} className='w-full' />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        )) : searchTerm ? (
+                            <div className='text-center'>No results found</div>
+                        ) : (
+                            <div className='text-center'>No matches found</div>
+                        )}
                     </div>
-                ))}
-            </div>
+
+                    <div className='mt-5'>
+                        <Pagination
+                            totalMatches={high.length}
+                            matchesPerPage={9}
+                            setCurrentPage={setCurrentPage}
+                            currentPage={currentPage}
+                        />
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Stream;
