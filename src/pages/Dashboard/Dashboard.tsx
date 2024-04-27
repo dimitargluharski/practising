65c1239bdbbf27108a45b1ec23315fc01a6aea9d
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-04-27 11:40:04 +0300

    Remove unused import of Highlight component in Dashboard.tsx

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,8 +4,6 @@ import { PiTelevisionSimpleLight } from "react-icons/pi";
 import { DashboardCards } from "../../components/DashbaordCards/DashboardCards";
 import { Slider } from "../../components/Swiper/Swiper";
 
-import { Highlight } from "../../components/Highlights/Highlight";
-
 const Dashboard = () => {
     const [data, setData] = useState<any>([]);
 
