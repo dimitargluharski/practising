8f230506b88a3bd088f65cd69f8046b3947a37cd
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-06-06 08:35:34 +0300

    improvements

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,7 +20,7 @@ export const Home = ({ query }: any) => {
   const [matches, setMatches] = useState<Match[]>([]);
   const [filteredMatches, setFilteredMatches] = useState<Match[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  
+
   const { grid } = useContext(GridContext);
 
   useEffect(() => {
@@ -50,18 +50,22 @@ export const Home = ({ query }: any) => {
   }, [query, matches]);
 
   const Loading = () => {
-    return(
+    return (
       <div>Fetching data...</div>
     )
   }
 
   return (
     <div className={`${grid === 'grid' ? 'grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 w-full' : 'flex flex-col w-[768px]'}`}>
-      {loading ? <Loading/> : filteredMatches.map((m, i) => (
-          <div key={i + 1 * Math.random()}>
-            <MatchCard matchData={m} />
-          </div>
-        ))}
+      {matches.length === 0
+        ? 'There is no live matches'
+        : loading
+          ? <Loading />
+          : filteredMatches.map((m, i) => (
+            <div key={i + 1 * Math.random()}>
+              <MatchCard matchData={m} />
+            </div>
+          ))}
     </div>
   )
 }
