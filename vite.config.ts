252a874568ff36edc8068589fc415eb023af1a72
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-01-11 09:34:25 +0200

    install new packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  server: {
-    open: true,
-  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+});
