6e1ed5b8e1c20d7eaf095fd8a5a409e79ea36847
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-01-18 12:17:49 +0200

    create input field component

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,11 @@
+export const InputField = () => {
+  return (
+    <input
+      type="text"
+      className="w-full"
+      placeholder="Search..."
+    // onChange={handleInputChangeHandler}
+    // value={query}
+    />
+  )
+}
